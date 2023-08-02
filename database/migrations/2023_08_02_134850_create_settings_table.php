d424a51ce5b3f63e4a766d827f9451338a155200
Konstantin S. Filin <info@kfilin.ru>
2023-08-02 17:56:52 +0300

    Module skeleton

---
+++ database/migrations/2023_08_02_134850_create_settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('larasettings', function (Blueprint $table) {
+            $table->integerIncrements('id')->unsigned();
+            $table->string('key')->unique();
+            $table->string('value')->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('larasettings');
+    }
+};
---
+++ src/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace KonstantinFilin\LaraSettings\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+            ]);
+        }
+    }
+}
